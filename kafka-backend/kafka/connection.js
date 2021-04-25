08462cc20d4214427384b6bf6b66235c4e0504cb
payalghule <payalghule@gmail.com>
2021-04-25 14:11:36 -0700

    kafkaURI constant

--- kafka-backend/kafka/connection.js
+++ kafka-backend/kafka/connection.js
@@ -1,10 +1,11 @@
 var kafka = require("kafka-node");
 var http = require("http");
+const Config = require("../config");
 
 function ConnectionProvider() {
 	this.getConsumer = function (topic_name) {
 		console.log("consumer consumer", topic_name);
-		this.client = new kafka.KafkaClient("localhost:2181");
+		this.client = new kafka.KafkaClient(`${Config.kafkaURI}`);
 		this.kafkaConsumerConnection = new kafka.Consumer(this.client, [
 			{ topic: topic_name, partition: 0 },
 		]);
@@ -18,7 +19,7 @@ function ConnectionProvider() {
 	//Code will be executed when we start Producer
 	this.getProducer = function () {
 		if (!this.kafkaProducerConnection) {
-			this.client = new kafka.KafkaClient("localhost:2181");
+			this.client = new kafka.KafkaClient(`${Config.kafkaURI}`);
 			var HighLevelProducer = kafka.HighLevelProducer;
 			this.kafkaProducerConnection = new HighLevelProducer(this.client);
 			console.log("producer ready");
