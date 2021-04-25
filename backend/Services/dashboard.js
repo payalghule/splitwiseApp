8042d3ad9d4cc3c14dbdec889c23bad1cef06a41
payalghule <payalghule@gmail.com>
2021-04-25 15:21:30 -0700

    checkauth revert

--- backend/Services/dashboard.js
+++ backend/Services/dashboard.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express();
 const kafka = require("../kafka/client");
-const { checkAuth } = require("../passport");
+//const { checkAuth } = require("../passport");
 
-router.post("/getdashdata", checkAuth, async (req, res) => {
+router.post("/getdashdata", async (req, res) => {
 	console.log("Backend:: inside getdashdata");
 	console.log("req.body :", req.body);
 	kafka.make_request("getdashdata", req.body, (err, results) => {
